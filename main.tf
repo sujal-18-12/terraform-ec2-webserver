e8b7a3a94934c650b15c64c667a2dcc63a4a4930
KOPPULA SUJAL KUMAR <koppulasujal@gmail.com>
2026-06-18 11:40:11 +0530

    Initial Terraform EC2 Web Server Deployment Project

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "web_sg" {
+  name = "web-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = var.instance_type
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "Terraform-Web-Server"
+  }
+}
